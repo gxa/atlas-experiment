092d265ea77150924ae63c5aaa32270ca4c5835d
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-09 13:55:40 +0100

    Downloads instead of Download (as per the story description) [#144694521]

--- src/tabs/experiment-design/Main.jsx
+++ src/tabs/experiment-design/Main.jsx
@@ -13,7 +13,7 @@ const ExperimentDesignTab = ({
           <div style={{textAlign:"right"}}>
             <a className="button" style={{margin:"0px"}} href={downloadUrl}>
               <span className="glyphicon glyphicon-download-alt" style={{marginRight:"0.5rem"}}/>
-              Download
+              Downloads
             </a>
           </div>
         </div>
