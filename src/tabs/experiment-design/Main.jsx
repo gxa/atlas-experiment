254d4b27232fab574f51ccce3b0b56af9d77db2e
wbazant <wbazant@ebi.ac.uk>
2017-05-12 10:49:15 +0100

    downloadUrl in the experiment design tab is actually relative though the back end developers didn't say [Fixes #145297809]

--- src/tabs/experiment-design/Main.jsx
+++ src/tabs/experiment-design/Main.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import TablePropTypes from './ExperimentDesignTablePropTypes.js'
 import {DifferentialExperimentDesign, BaselineExperimentDesign} from './ExperimentDesignTable.jsx'
+import URI from 'urijs'
 
 const ExperimentDesignTab = ({
   isDifferential,
   downloadUrl,
+  atlasUrl,
   table
 }) => (
     <div>
       <div className="row">
         <div className="small-12 columns margin-top-large">
           <div style={{textAlign:"right"}}>
-            <a className="button" style={{margin:"0px"}} href={downloadUrl}>
+            <a className="button" style={{margin:"0px"}} href={URI(downloadUrl, atlasUrl).toString()}>
               <span className="glyphicon glyphicon-download-alt" style={{marginRight:"0.5rem"}}/>
               Download
             </a>
@@ -33,6 +35,7 @@ const ExperimentDesignTab = ({
 ExperimentDesignTab.propTypes = {
   isDifferential : React.PropTypes.bool.isRequired,
   downloadUrl: React.PropTypes.string.isRequired,
+  atlasUrl: React.PropTypes.string.isRequired,
   table: React.PropTypes.shape(TablePropTypes)
 }
 
