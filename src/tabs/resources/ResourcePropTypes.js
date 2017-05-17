2974e1dc3f14f4d6118ec0bf17e3cca411f1975e
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2017-05-17 16:42:17 +0100

    Add prop atlasUrl

--- src/tabs/resources/ResourcePropTypes.js
+++ src/tabs/resources/ResourcePropTypes.js
@@ -5,5 +5,6 @@ export default {
   description: React.PropTypes.string.isRequired,
   group: React.PropTypes.string.isRequired,
   type: React.PropTypes.string.isRequired,
+  atlasUrl: React.PropTypes.string.isRequired,
   url: React.PropTypes.string.isRequired
 }
