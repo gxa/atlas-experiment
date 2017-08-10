ff2d3bf049a28893504edc1a1686eaca8f139bba
barrera <barrera@ebi.ac.uk>
2017-08-10 09:37:05 +0100

    Change Header to Uppercase in React-table to show the headers

--- src/tabs/experiment-design/ExperimentDesignTable.js
+++ src/tabs/experiment-design/ExperimentDesignTable.js
@@ -27,11 +27,11 @@ const ExperimentDesign = ({
     columns={
       headers.map((headerGroup,ix)=> (
         {
-          header: headerGroup.name,
+          Header: headerGroup.name,
           columns:
             headerGroup.values.map((header, jx) => ({
               aggregate: curry(aggregateText, 2)(header),
-              header: header,
+              Header: header,
               id: ix*1000 +jx +1,
               accessor: r => r.values[ix][jx],
             }))
